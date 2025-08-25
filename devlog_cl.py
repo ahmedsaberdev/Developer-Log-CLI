49832b821a9960456771e2d638ce7f27f39c529a
Ahmed Saber <ahmed.saber.dev.05@gmail.com>
2025-08-25 15:03:12 +0300

    feature: add time (HH:MM:SS) for different entrys in the same date. feature: add configurations for date and time format for customizability.

--- devlog_cl.py
+++ devlog_cl.py
@@ -1,3 +1,4 @@
+import json
 import argparse
 from pathlib import Path
 from datetime import date, datetime
@@ -6,13 +7,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument('entry', type=str, help='add entry to devlog file')
 args = parser.parse_args()
 
+with open('config.json', 'r') as f:
+  config = json.load(f)
+
+DATE_FORMAT = config['date-format']
+TIME_FORMAT = config['time-format']
+
 def add_entry(file: str, entry: str):
-  today_date = date.today().strftime('%d-%m-%Y')
+  today_date = date.today().strftime(DATE_FORMAT)
+  now_time = datetime.now().strftime(TIME_FORMAT)
   file_path = Path(file)
 
   if not file_path.exists():
     with file_path.open('w') as f:
-      f.write(f'{today_date}\n - {entry}')
+      f.write(f'{today_date}\n - ({now_time})\n    {entry}\n')
     return
 
   with file_path.open('r') as f:
@@ -20,10 +28,10 @@ def add_entry(file: str, entry: str):
 
   if any(line.strip() == today_date for line in lines):
     with file_path.open('a') as f:
-      f.write(f' - {entry}')
+      f.write(f'- ({now_time})\n    {entry}\n')
   else:
     with file_path.open('a') as f:
-      f.write(f'{today_date}\n - {entry}')
+      f.write(f'{today_date}\n - ({now_time})\n    {entry}\n')
 
 
 if __name__ == "__main__":
